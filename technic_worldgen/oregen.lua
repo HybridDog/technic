fe2c6562bafeded9c0f68b3a4050485d4eabc1b4
HybridDog <adresse_test@t-online.de>
2015-08-16 13:20:12 +0200

    Tidy up sulfur mapgen code

--- technic_worldgen/oregen.lua
+++ technic_worldgen/oregen.lua
@@ -123,10 +123,7 @@ local sulfur_noise= nil
 
 minetest.register_on_generated(function(minp, maxp, seed)
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
-	local a = VoxelArea:new{
-		MinEdge = {x = emin.x, y = emin.y, z = emin.z},
-		MaxEdge = {x = emax.x, y = emax.y, z = emax.z},
-	}
+	local a = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
 	local data = vm:get_data(sulfur_buf)
 	local pr = PseudoRandom(17 * minp.x + 42 * minp.y + 101 * minp.z)
 	sulfur_noise = sulfur_noise or minetest.get_perlin(9876, 3, 0.5, 100)
@@ -141,12 +138,14 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	for y = minp.y + math.floor(grid_size / 2), maxp.y, grid_size do
 	for z = minp.z + math.floor(grid_size / 2), maxp.z, grid_size do
 		local c = data[a:index(x, y, z)]
-		if (c == c_lava or c == c_lava_flowing) and sulfur_noise:get3d({x = x, y = z, z = z}) >= 0.4 then
+		if (c == c_lava or c == c_lava_flowing)
+		and sulfur_noise:get3d({x = x, y = z, z = z}) >= 0.4 then
 			for xx = math.max(minp.x, x - grid_size), math.min(maxp.x, x + grid_size) do
 			for yy = math.max(minp.y, y - grid_size), math.min(maxp.y, y + grid_size) do
 			for zz = math.max(minp.z, z - grid_size), math.min(maxp.z, z + grid_size) do
 				local i = a:index(xx, yy, zz)
-				if data[i] == c_stone and pr:next(1, 10) <= 7 then
+				if data[i] == c_stone
+				and pr:next(1, 10) <= 7 then
 					data[i] = c_sulfur
 				end
 			end
@@ -163,32 +162,33 @@ end)
 
 
 if technic.config:get_bool("enable_marble_generation") then
-minetest.register_ore({
-	ore_type       = "sheet",
-	ore            = "technic:marble",
-	wherein        = "default:stone",
-	clust_scarcity = 1,
-	clust_num_ores = 1,
-	clust_size     = 3,
-	y_min     = -31000,
-	y_max     = -50,
-	noise_threshold = 0.4,
-	noise_params = {offset=0, scale=15, spread={x=150, y=150, z=150}, seed=23, octaves=3, persist=0.70}
-})
+	minetest.register_ore({
+		ore_type       = "sheet",
+		ore            = "technic:marble",
+		wherein        = "default:stone",
+		clust_scarcity = 1,
+		clust_num_ores = 1,
+		clust_size     = 3,
+		y_min     = -31000,
+		y_max     = -50,
+		noise_threshold = 0.4,
+		noise_params = {offset=0, scale=15, spread={x=150, y=150, z=150},
+			seed=23, octaves=3, persist=0.70}
+	})
 end
 
 if technic.config:get_bool("enable_granite_generation") then
-minetest.register_ore({
-	ore_type       = "sheet",
-	ore            = "technic:granite",
-	wherein        = "default:stone",
-	clust_scarcity = 1,
-	clust_num_ores = 1,
-	clust_size     = 4,
-	y_min     = -31000,
-	y_max     = -150,
-	noise_threshold = 0.4,
-	noise_params = {offset=0, scale=15, spread={x=130, y=130, z=130}, seed=24, octaves=3, persist=0.70}
-})
+	minetest.register_ore({
+		ore_type       = "sheet",
+		ore            = "technic:granite",
+		wherein        = "default:stone",
+		clust_scarcity = 1,
+		clust_num_ores = 1,
+		clust_size     = 4,
+		y_min     = -31000,
+		y_max     = -150,
+		noise_threshold = 0.4,
+		noise_params = {offset=0, scale=15, spread={x=130, y=130, z=130},
+			seed=24, octaves=3, persist=0.70}
+	})
 end
-
